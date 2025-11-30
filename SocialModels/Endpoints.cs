78eca5b69a312fbfa226b4ae3ce63283e45e82f4
Panagiotis Katsampiris <lostinmyworld222@gmail.com>
2025-12-01 00:01:12 +0200

    - changed Endpoints accessibility level

--- SocialModels/Endpoints.cs
+++ SocialModels/Endpoints.cs
@@ -1,6 +1,6 @@
 ﻿namespace SocialModels;
 
-internal static class Endpoints
+public static class Endpoints
 {
     public const string InstagramGraphApiUri = "https://graph.instagram.com/me/media";
     public const string GithubGistApiUri = "https://api.github.com/gists/{gistId}";
