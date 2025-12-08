c93aeedea7528ae364d17bb4862bc37c1e40f99b
Panagiotis Katsampiris <lostinmyworld222@gmail.com>
2025-12-08 21:04:36 +0200

    added GistOptions

---
+++ SocialModels/GistOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialModels;
+
+public class GistOptions
+{
+    public string GistId { get; set; } = default!;
+    public string GistStateFileName { get; set; } = default!;
+    public string GistToken { get; set; } = default!;
+}
